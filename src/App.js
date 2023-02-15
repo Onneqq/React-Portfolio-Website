5d2e18184b5bca4d05d7a1f959605bae6de48ab6
Onneqq <adampenno93@gmail.com>
2023-02-16 10:45:26 +1100

    firebase set up

--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ import Portfolio from './components/Portfolio';
 import ContactUs from './components/ContactUs';
 import Footer from './components/Footer';
 import resumeData from './resumeData';
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAr4tY8JZI1UpKIhOw5NxyirUnO9U89gHA",
+  authDomain: "adam-penno-portfolio-react.firebaseapp.com",
+  projectId: "adam-penno-portfolio-react",
+  storageBucket: "adam-penno-portfolio-react.appspot.com",
+  messagingSenderId: "581065537031",
+  appId: "1:581065537031:web:5235f13a40c2bce3ae5ca2",
+  measurementId: "G-93MB4TFZ3X"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 class App extends Component {
   render() {
     return (
